0284695fc2b61cd753139e58d2ed678106c1c4e4
alvaro <aaruizf@unal.edu.co>
2021-04-08 14:10:25 -0500

    Add basic function to display data

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-api_key = 'b863a920259d022cf48f60daf142113b'
-city='colombia'
-units = '°C'
-const forecast = async(city,units='°C',lang='en') => {
+// api_key = 'b863a920259d022cf48f60daf142113b'
+
+
+const forecast = async(place,units='°C',lang='en')  => {
   await fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&lang=${lang}&appid=b863a920259d022cf48f60daf142113b`).then(function(response) {
+  `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=${units}&lang=${lang}&appid=b863a920259d022cf48f60daf142113b`).then(function(response) {
     return response.json()
-}).then(res => console.log(res));
+}).then(res => res).then((data) => displayData(data) );
 }
 
-forecast('colombia')
-"https://api.openweathermap.org/data/2.5/weather?q=${place}&lang=en&units=${unit}&appid=96234945f21abe7a841c8fc0306c59be"
+// forecast('colombia')
+
+const start = () => {
+  const place = document.getElementById('place').value;
+  const units = document.getElementById('units').value;
+  const language = document.getElementById('language').value
+  forecast(place, units, language);
+
+}
+
+
+// forecast(place,units,language)
+displayData = (data) => {
+  // console.log('Here');
+  console.log(data.weather[0]);
+  // console.log(place);
+  // const data = forecast(place, units, language);
+  const icon = document.getElementById('icon');
+  const description = document.getElementById('description');
+  description.textContent = data.weather[0].description;
+  icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+  const wind = document.getElementById('wind');
+  wind.textContent = data.wind.speed;
+  const temp = document.getElementById('temp');
+  temp.textContent = data.main.temp_max
+}
+
