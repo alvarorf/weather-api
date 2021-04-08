fc6f98d0a62a9defc169996972d9a23f2af3f273
alvaro <aaruizf@unal.edu.co>
2021-04-08 16:24:35 -0500

    Add some styling and modify function to display data

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
 // api_key = 'b863a920259d022cf48f60daf142113b'
 
 
-const forecast = async(place,units='°C',lang='en')  => {
+const forecast = async(place,units)  => {
+  console.log(units);
   await fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=${units}&lang=${lang}&appid=b863a920259d022cf48f60daf142113b`).then(function(response) {
+  `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=${units}&lang=en&appid=b863a920259d022cf48f60daf142113b`).then(function(response) {
     return response.json()
 }).then(res => res).then((data) => displayData(data) );
 }
 
 // forecast('colombia')
-
+const place = document.getElementById('place').value;
+const units = document.getElementById('units').value;
 const start = () => {
-  const place = document.getElementById('place').value;
-  const units = document.getElementById('units').value;
-  const language = document.getElementById('language').value
-  forecast(place, units, language);
+  forecast(place, units);
 
 }
 
 
 // forecast(place,units,language)
 displayData = (data) => {
-  // console.log('Here');
-  console.log(data.weather[0]);
-  // console.log(place);
-  // const data = forecast(place, units, language);
   const icon = document.getElementById('icon');
   const description = document.getElementById('description');
   description.textContent = data.weather[0].description;
   icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
-  const wind = document.getElementById('wind');
-  wind.textContent = data.wind.speed;
   const temp = document.getElementById('temp');
-  temp.textContent = data.main.temp_max
+  const wind = document.getElementById('wind');
+  const hum = document.getElementById('hum');
+  const results = convertData (data);
+  temp.textContent = results[0];
+  wind.textContent = results[1];
+  hum.textContent = results[2];
 }
 
+const convertData = (data) => {
+  let tempd = Math.round(100*(((data.main.temp_max + data.main.temp_min)/2) - 273.15))/100;
+  let windd = Math.round(data.wind.speed*100)/100;
+  let humd = Math.round(data.main.humidity*100)/100;
+  console.log(humd)
+  if(units == '°F'){
+    tempd = ((9/5)*tempd + 32) + ' °F';
+    windd = Math.round(windd*2.23694*100)/100 + ' miles/hour'
+  }
+  else {
+    tempd = tempd + ' °C';
+    windd = windd + ' meters/second';
+  }
+  return [tempd, windd, humd];
+}
