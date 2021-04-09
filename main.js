84c2cc2fc5f56ee6fbf4e7a6e5d8dfa493712347
alvaro <aaruizf@unal.edu.co>
2021-04-09 17:03:11 -0500

    Update live version

--- main.js
+++ main.js
@@ -1,1 +1,158 @@
-(()=>{"use strict";var e={};(e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})})(e);const t=document.createElement("input");t.id="place",document.getElementById("enter-place").appendChild(t),document.getElementById("search").addEventListener("click",(()=>(()=>{const t=document.getElementById("units").value;document.getElementById("icon").src="./giphy.gif";const n=document.getElementById("place").value;e.forecast(n,t)})()))})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/logic.js":
+/*!**********************!*\
+  !*** ./src/logic.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* eslint-disable no-use-before-define */
+const displayData = (data, units) => {
+  const giphy = document.getElementById('giphy');
+  const icon = document.getElementById('icon');
+  const description = document.getElementById('description');
+  description.textContent = data.weather[0].description;
+  getGiphy(description, giphy);
+  icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+  const temp = document.getElementById('temp');
+  const wind = document.getElementById('wind');
+  const hum = document.getElementById('hum');
+  const pre = document.getElementById('pre');
+  [temp.textContent, wind.textContent, hum.textContent, pre.textContent] = convertData(
+    data, units,
+  );
+};
+/* eslint-enable no-use-before-define */
+
+const forecast = async (place, units) => {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=${units}&lang=en&appid=b863a920259d022cf48f60daf142113b`,
+  ).then(
+    (response) => response.json(),
+  ).then((res) => res).then((data) => displayData(data, units));
+};
+
+const convertData = (data, units) => {
+  let tempd = Math.round(((data.main.temp_max + data.main.temp_min) / 2) * 100) / 100;
+  let windd = Math.round(data.wind.speed * 100) / 100;
+  const humd = Math.round(data.main.humidity * 100) / 100;
+  const pre = data.main.pressure;
+  if (units === 'metric') {
+    tempd += ' °C';
+    windd += ' meters/second';
+  } else {
+    tempd += ' °F';
+    windd += ' miles/hour';
+  }
+
+  return [tempd, windd, humd, pre];
+};
+
+const getGiphy = async (search, giphy) => {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=2EwOKoQXnNF8ez8G3i67obeO2o5TT7EN&s=${search}`, { mode: 'cors' });
+    const weatherImgData = await response.json();
+    giphy.src = weatherImgData.data.images.original.url;
+    giphy.style.borderStyle = 'none';
+  } catch (error) {
+    giphy.src = '#';
+  }
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (forecast);
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _logic__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./logic */ "./src/logic.js");
+
+
+const placev = document.createElement('input');
+placev.id = 'place';
+document.getElementById('enter-place').appendChild(placev);
+
+const startP = () => {
+  const units = document.getElementById('units').value;
+  const icon = document.getElementById('icon');
+  icon.src = './giphy.gif';
+  const place = document.getElementById('place').value;
+  (0,_logic__WEBPACK_IMPORTED_MODULE_0__.default)(place, units);
+};
+
+const button = document.getElementById('search');
+button.addEventListener('click', () => startP());
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
