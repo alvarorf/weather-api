fa0d258b80f6eb48caf78ebb908eda379cd012e5
alvaro <aaruizf@unal.edu.co>
2021-04-09 16:59:44 -0500

    Fix remaining bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as logic from './logic';
+import forecast from './logic';
 
 const placev = document.createElement('input');
 placev.id = 'place';
@@ -9,7 +9,7 @@ const startP = () => {
   const icon = document.getElementById('icon');
   icon.src = './giphy.gif';
   const place = document.getElementById('place').value;
-  logic.forecast(place, units);
+  forecast(place, units);
 };
 
 const button = document.getElementById('search');
